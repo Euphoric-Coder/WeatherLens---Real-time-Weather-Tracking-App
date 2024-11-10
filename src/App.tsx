3e757eb7e3205f9078158e63c360c99c7124769a
Multiprocesssing-CODER <sagnik.dey745@icloud.com>
2024-11-11 04:36:06 +0530

    Gave the basic layout of the web app (header, footer and set up the layout)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import WeatherDashboard from "./components/WeatherDashboard";
+import CityPage from "./components/CityPage";
+import { Layout } from "./components/layout";
 
 const App = () => {
   return (
-    <div>
-      Lorem ipsum dolor sit amet.
-    </div>
-  )
-}
+    <BrowserRouter>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<WeatherDashboard />} />
+        <Route path="/city/:cityName" element={<CityPage />} />
+      </Routes>
+    </Layout>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
